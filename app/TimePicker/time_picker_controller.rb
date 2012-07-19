be8201f51254e26f7aaaea19bee721c9e6845639
tomomura <am.n04a@gmail.com>
2012-07-19 10:31:55 +0900

    範囲指定時間選択機能実装

--- app/TimePicker/time_picker_controller.rb
+++ app/TimePicker/time_picker_controller.rb
@@ -13,24 +13,50 @@ class TimePickerController < Rho::RhoController
   #タイムピッカー機能の呼び出し
   def choose
     DateTimePicker.choose(url_for(:action => :choose_callback),
-                          '時間を選択して下さい',
+                          '選択して下さい',
                           Time.new,
                           @params['flg'].to_i,
                           Marshal.dump(@params['flg'])
                          )
-
+    redirect :action => :index
   end
 
   def choose_callback
     if @params['status'] == 'ok'
+      time = Time.at(@params['result'].to_i)
+      flg = Marshal.load(@params['opaque'])
+      case flg
+      when "0" then
+        format = '%F %T'
+      when "1"
+        format = '%F'
+      when "2"
+        format = '%T'
+      else
+        format = '%F %T'
+      end
       Alert.show_popup(
         #表示するメッセージを指定
-        :message => "#{Time.at(@params['result'].to_i)}",
+        :message => "#{time.strftime(format)}",
         #ポップアップのタイトルを指定
-        :title => "お知らせ",
+        :title => "あなたが選択した時間",
         :buttons => ["了解"]
       )
     end
     WebView.navigate(url_for(:action => :index))
   end
+
+  def choose_range
+    DateTimePicker.choose_with_range(url_for(:action => :choose_callback),
+                                     '選択して下さい',
+                                     Time.new,
+                                     @params['format'].to_i,
+                                     Marshal.dump(@params['format']),
+                                     Time.now,
+                                     Time.now + 31536000
+                                     )
+    redirect :action => :index
+  end
+
+
 end
