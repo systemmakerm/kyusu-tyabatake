759debbd6d385867860fb0f8b0a23ddff712c659
nishima <nishima@nitetsu-air.local>
2012-07-19 17:29:27 +0900

    [Mod] XML

--- app/XmlTest/xml_test_controller.rb
+++ app/XmlTest/xml_test_controller.rb
@@ -1,13 +1,10 @@
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
-require 'helpers/application_helper'
 require 'rexml/document'
 
 # XML機能
 class XmlTestController < Rho::RhoController
   include BrowserHelper
-  include ApplicationHelper
-
   
   # XML機能トップメニュー
   def index
@@ -18,16 +15,20 @@ class XmlTestController < Rho::RhoController
   def parse_xml
     file_name = File.join(Rho::RhoApplication::get_model_path('app','XmlTest'), 'sample.xml')
     file = File.new(file_name)
+    # REXML::Document.new(str)
+    # * str :: 解析したい文字列 
     @xml = REXML::Document.new(file)
     render :back => url_for(:action => :index)
   end
   
   # RSSを解析して表示する。
   def show_rss
+    # Yahoo!からニュース情報をRSSで取得する。
     http = Rho::AsyncHttp.get(:url => "http://rss.dailynews.yahoo.co.jp/fc/sports/rss.xml")
     if http["status"] == "ok"
       # HTTPアクセス成功時
       @xml = REXML::Document.new(http["body"])
+      render :back => url_for(:action => :index)
     else
       # HTTPアクセス失敗時
       Alert.show_popup("サーバからRSSを取得できませんでした。")
