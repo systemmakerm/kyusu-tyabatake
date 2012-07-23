1239614deeae5b1e91324d602b5b75303fcbd3da
tomomura <am.n04a@gmail.com>
2012-07-23 15:35:29 +0900

    機能紹介のバリエーションを追加

--- app/Rhotimer/rhotimer_controller.rb
+++ app/Rhotimer/rhotimer_controller.rb
@@ -12,7 +12,13 @@ class RhotimerController < Rho::RhoController
   #タイマーをスタートさせる
   def timer_start
     #タイマー機能スタート(時間(ミリ), コールバックURL, コールバックへ渡す値)
-    Rho::Timer.start(5000, url_for(:action => :timer_callback), 'test')
+    Rho::Timer.start(5000, url_for(:action => :timer_callback), "type=#{@params['type']}")
+    case @params['type']
+    when "1"
+      @msg = "5秒後に元の画面へ戻ります。"
+    when "2"
+      @msg = "5秒後にポップアップが表示されます。"
+    end
     render :action => :wait
   end
 
@@ -25,6 +31,13 @@ class RhotimerController < Rho::RhoController
 
   #タイマーのコールバック
   def timer_callback
+    if @params['type'] == "2"
+      Alert.show_popup(
+        :message => "5秒経過しました",
+        :title => "お知らせ",
+        :buttons => ["同意"],
+      )
+    end
     WebView.navigate(url_for(:action => :index))
   end
 end
